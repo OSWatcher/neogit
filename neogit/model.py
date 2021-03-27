3b1a4fca88ce45566ad79ab74a23a94cf2f2a511
Mathieu Tarral <mathieu.tarral@protonmail.com>
2021-03-27 09:03:03 +0100

    model: add TreeNode and BlobNode

---
+++ neogit/model.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+from typing import Dict, Union
+
+
+@dataclass(init=False)
+class BlobNode:
+    sha1sum: str
+
+
+@dataclass(init=False)
+class TreeNode:
+    sha1sum: str
+    children: Dict[str, Union[BlobNode, "TreeNode"]]
+
+    def __init__(self):
+        self.children = {}
