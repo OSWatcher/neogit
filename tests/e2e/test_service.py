c469c2176a8d5ddd2d66d92a3c0665773b887ee4
Mathieu Tarral <mathieu.tarral@protonmail.com>
2021-03-30 00:06:53 +0200

    tests: add simple e2e test

---
+++ tests/e2e/test_service.py
@@ -0,0 +1,32 @@
+from neogit.model import Branch
+from neogit.repo.py2neo import Py2NeoRepository
+from neogit.service import Neogit
+from tests.conftest import TEST_DATA_FS
+
+
+def test_single_commit_dir_empty(py2neo_repo: Py2NeoRepository):
+    empty_dir_path = TEST_DATA_FS / "dir_empty"
+    neogit = Neogit(empty_dir_path, py2neo_repo)
+    branch_name = "master"
+    commit_name = "first_commit"
+
+    neogit.commit(commit_name)
+
+    # branch master should have been created
+    m = py2neo_repo.match(Branch)
+    assert m.count() == 1
+    branch = m.first()
+    assert branch.name == branch_name
+    # master should point to first commit
+    commits = list(branch.commit)
+    assert len(commits) == 1
+    commit = commits[0]
+    assert commit.name == commit_name
+    # first commit should have no previous
+    assert not list(commit.previous_commit)
+    # first commit should point to Tree node
+    trees = list(commit.filesystem)
+    assert len(trees) == 1
+    tree = trees[0]
+    # tree node should have empty dir sha1
+    assert "da39a3ee5e6b4b0d3255bfef95601890afd80709", tree.sha1sum
