170a6070a08a36f8acbb4896c297352012a4877f
Mathieu Tarral <mathieu.tarral@protonmail.com>
2021-03-30 20:41:37 +0200

    tests/dev: add test_commit_dir

--- tests/dev/test_dev.py
+++ tests/dev/test_dev.py
@@ -1,10 +1,22 @@
+from pathlib import Path
+
 import pytest
 from py2neo.ogm import Repository
 
 from tests.conftest import Neo4jConnection
+from neogit.service import Neogit
 
 
 @pytest.mark.dev
 def test_py2neo_connection(neo4j_con: Neo4jConnection):
     repo = Repository(neo4j_con.to_bolt(crendentials=True))  # noqa: F841
+    raise AssertionError()
+
+
+@pytest.mark.dev
+def test_commit_dir(py2neo_repo):
+    path = Path("")
+    neogit = Neogit(path, py2neo_repo)
+
+    neogit.commit("first commit")
     raise AssertionError()
