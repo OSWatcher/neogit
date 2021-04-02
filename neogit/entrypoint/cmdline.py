600da7b8757a745c03955244673056d0ee866fc5
Mathieu Tarral <mathieu.tarral@protonmail.com>
2021-04-02 23:10:47 +0200

    cmdline: add debug option to toggle logging output

--- neogit/entrypoint/cmdline.py
+++ neogit/entrypoint/cmdline.py
@@ -7,15 +7,24 @@ Options:
   -h --help             Show this screen.
   --version             Show version.
   -r ROOT --root=ROOT   Specify repo root directory
+  -d --debug            Toogle debug output
 """
+
 from docopt import docopt
 from neogit.service import Neogit
 from pathlib import Path
 from neogit.repo import Py2NeoRepository
+import logging
 
 
 def handle_cmdline():
     args = docopt(__doc__)
+    log_lvl = logging.INFO
+    if args["--debug"]:
+        log_lvl = logging.DEBUG
+    logging.basicConfig(level=log_lvl)
+    # silence py2neo
+    logging.getLogger("py2neo.client").setLevel(logging.WARNING)
     # handle root
     root_repo: Path = Path.cwd()
     if args["--root"]:
