85b6a7e4585369fbe0abaefbb1f7fb3f2e14eb9b
Mathieu Tarral <mathieu.tarral@protonmail.com>
2021-04-28 21:14:09 +0200

    merkle: implement new MerkleTree builder based on ThreadPool

--- neogit/merkle/angela.py
+++ neogit/merkle/angela.py
@@ -1,53 +1,68 @@
 import logging
 import os
-import pickle
-from multiprocessing import Queue
 from pathlib import Path
-from typing import Dict, Tuple
+from typing import Dict, Tuple, Optional, List
+from concurrent.futures import ThreadPoolExecutor, Future, as_completed
+from queue import Queue
+from threading import Thread
 
-from neogit.merkle.proc import MerkleWorker
-from neogit.merkle.utils import merkelize_dir
+
 from neogit.model import Tree
-
-DEFAULT_MAX_WORKERS = 4
-
-
-def iterable_queue(queue: Queue):
-    while True:
-        item = queue.get()
-        if item is None:
-            return
-        yield item
+from neogit.config import settings
+from neogit.merkle.utils import merkelize_file, merkelize_dir
 
 
 class MerkleFSTree:
-    def __init__(self, root_fs: Path, max_workers: int = None):
+    def __init__(self, root_fs: Path):
         if not root_fs.exists():
             raise ValueError(f"root {root_fs} does not exists")
-        self._logger = logging.getLogger(f"{MerkleFSTree.__module__}.{MerkleFSTree.__class__.__name__}")
-        self._max_workers = max_workers if max_workers is not None else DEFAULT_MAX_WORKERS
-        self._root = root_fs
-        self._workers: Dict[Path, Tuple[MerkleWorker, Queue]] = {}
+        self._logger = logging.getLogger(f"{self.__module__}.{self.__class__.__name__}")
+        self._max_workers: Optional[int] = settings.get("max_workers")
+        self._root: Path = root_fs
+
+        self._expl_thread = Thread(target=self._explore_dfs, args=(self._root,), name="explore")
+        self._sha1_pool = ThreadPoolExecutor(self._max_workers, "sha1-pool")
+        self._task_queue: Queue = Queue()
+        self._tree_fs: Dict[Path, Tree] = {}
 
     def merkelize(self) -> Tree:
-        with os.scandir(self._root) as it:
-            for entry in it:
-                if entry.is_dir(follow_symlinks=False):
-                    entry_path = Path(entry.path)
-                    worker_queue: Queue = Queue()
-                    worker = MerkleWorker(entry_path, worker_queue)
-                    worker.start()
-                    self._workers[entry_path] = (worker, worker_queue)
-        tree_fs = {}
-        for w_path, (worker, w_queue) in self._workers.items():
-            worker.join()
-            logging.debug("waiting for results from %s", worker.name)
-            serial_path = w_queue.get()
-            with open(serial_path, mode="rb") as f:
-                node = pickle.load(f)
-            os.remove(serial_path)
-            logging.debug("results from %s", worker.name)
-            tree_fs[w_path] = node
-        root_node = merkelize_dir(self._root, tree_fs)
-        logging.info("📁 %s : %s", self._root, root_node.sha1sum)
-        return root_node
+        self._expl_thread.start()
+        while True:
+            task = self._task_queue.get()
+            if task is None:
+                break
+            cur_dir, filename_to_future, subdir_list = task
+            # wait for futures completion
+            filename_to_sha1: Dict[str, str] = {}
+            for file_sha1_fut in as_completed(list(filename_to_future.keys())):
+                filename: str = filename_to_future[file_sha1_fut]
+                filename_to_sha1[filename] = file_sha1_fut.result()
+            tree: Tree = merkelize_dir(cur_dir, filename_to_sha1, subdir_list, self._tree_fs)
+            self._logger.debug("📁 %s: %s", cur_dir, tree.sha1sum)
+            # update tree_fs
+            self._tree_fs[cur_dir] = tree
+        self._expl_thread.join()
+        # return root tree
+        return self._tree_fs[self._root]
+
+    def _explore_dfs(self, cur_dir: Path):
+        self._explore_dfs_rec(cur_dir)
+        # stop consuming tasks
+        self._task_queue.put(None)
+
+    def _explore_dfs_rec(self, cur_dir: Path):
+        with os.scandir(cur_dir) as it:
+            # process dirs first
+            entries = list(it)
+            subdir_list = []
+            for subdir in [entry for entry in entries if entry.is_dir(follow_symlinks=False)]:
+                subdir_list.append(subdir.name)
+                subdir_path = Path(subdir.path)
+                self._explore_dfs_rec(subdir_path)
+            future_to_filename: Dict[Future, str] = {}
+            for file in [entry for entry in entries if not entry.is_dir(follow_symlinks=False)]:
+                filepath = Path(file.path)
+                future = self._sha1_pool.submit(merkelize_file, filepath)
+                future_to_filename[future] = filepath.name
+            task: Tuple[Path, Dict[Future, str], List[str]] = (cur_dir, future_to_filename, subdir_list)
+            self._task_queue.put(task)
--- neogit/merkle/utils.py
+++ neogit/merkle/utils.py
@@ -1,63 +1,49 @@
 """This module contains utils functions to build a MerkleTree"""
 
 import logging
-import os
 from pathlib import Path
-from typing import Dict
+from typing import Dict, List
 
 from neogit.merkle.hasher import Hasher
 from neogit.model import Blob, Tree
+import threading
 
 
-def merkelize_file(filepath: Path) -> Blob:
-    """Create a BlobNode from a single file"""
-    blob = Blob()
+def merkelize_file(file: Path) -> str:
+    logger = logging.getLogger("PoolSHA1")
+    hash = Hasher()
+    hash.filepath(file)
+    tid = threading.get_ident()
+    sha1 = hash.digest()
+    logger.debug("[%s]📄 %s: %s", tid, file, hash.digest())
+    return sha1
+
+
+def merkelize_dir(
+    cur_dir: Path, filename_to_sha1: Dict[str, str], subdir_list: List[str], tree_fs: Dict[Path, Tree]
+) -> Tree:
+    tree: Tree = Tree()
+    for filename, sha1 in filename_to_sha1.items():
+        # create and insert a new Blob
+        blob = Blob()
+        blob.sha1sum = sha1
+        tree.children_blob[filename] = blob
+    for subdir_name in subdir_list:
+        subdir_path = cur_dir / subdir_name
+        tree.children_tree[subdir_name] = tree_fs[subdir_path]
+        del tree_fs[subdir_path]
+    # compute final hash for tree
     hasher = Hasher()
-    filepath = Path(filepath)
-    sha1sum = hasher.filepath(filepath).digest()
-    blob.sha1sum = sha1sum
-    return blob
-
-
-def merkelize_dir(directory: Path, tree_fs: Dict[Path, Tree]) -> Tree:
-    """
-    Convert a directory to a Merkel TreeNode object, given its subdirectories associated
-    TreeNodes in tree_fs
-
-    params:
-        directory: the directory to merkelize
-        tree_fs: the dict children filename -> TreeNode
-
-    return:
-        TreeNode
-    """
-    with os.scandir(directory) as it:
-        tree = Tree()
-        for entry in it:
-            if entry.is_file():
-                blob: Blob = merkelize_file(Path(entry.path))
-                # add to treenode
-                tree.children_blob[entry.name] = blob
-            if entry.is_dir(follow_symlinks=False):
-                entry_path = Path(entry.path)
-                assert entry_path in tree_fs
-                tree.children_tree[entry.name] = tree_fs[entry_path]
-                # remove entry from tree_fs to save RAM
-                del tree_fs[entry_path]
-        # compute final hash for tree
-        hasher = Hasher()
-        # IMPORTANT: sort the keys before using them
-        # directories first
-        for entry_name in sorted(tree.children_tree):
-            child_sha1sum = tree.children_tree[entry_name].sha1sum
-            logging.debug(child_sha1sum)
-            data = f"{entry_name}{child_sha1sum}\n"
-            hasher.string(data.encode())
-        logging.debug(hasher.digest())
-        # then files
-        for entry_name in sorted(tree.children_blob):
-            child_sha1sum = tree.children_blob[entry_name].sha1sum
-            data = f"{entry_name}{child_sha1sum}\n"
-            hasher.string(data.encode())
-        tree.sha1sum = hasher.digest()
-        return tree
+    # IMPORTANT: sort the keys before using them
+    # directories first
+    for entry_name in sorted(tree.children_tree):
+        child_sha1sum = tree.children_tree[entry_name].sha1sum
+        data = f"{entry_name}{child_sha1sum}\n"
+        hasher.string(data.encode())
+    # then files
+    for entry_name in sorted(tree.children_blob):
+        child_sha1sum = tree.children_blob[entry_name].sha1sum
+        data = f"{entry_name}{child_sha1sum}\n"
+        hasher.string(data.encode())
+    tree.sha1sum = hasher.digest()
+    return tree
