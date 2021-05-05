5abd374a08aa0cd7555ce21efdb8b988ff15ad30
Mathieu Tarral <mathieu.tarral@protonmail.com>
2021-05-05 22:10:13 +0200

    object_storage: add object storage definitions and adapters

---
+++ neogit/object_storage/abstract.py
@@ -0,0 +1,58 @@
+"""Defines the interface to the Object Storage"""
+
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class StorageDriver:
+    pass
+
+
+@dataclass(frozen=True)
+class Container:
+    name: str
+
+
+@dataclass
+class Object:
+    name: str
+    size: int
+    hash: str
+    container: Container
+    extra: dict
+    meta_data: dict
+
+
+class ContainerError(Exception):
+    pass
+
+
+class ContainerAlreadyExists(ContainerError):
+    pass
+
+
+class ContainerDoesNotExistError(ContainerError):
+    pass
+
+
+class AbstractObjectStorage(ABC):
+    @abstractmethod
+    def create_container(self, name: str) -> Container:
+        pass
+
+    @abstractmethod
+    def get_container(self, name: str) -> Container:
+        pass
+
+    @abstractmethod
+    def upload_object(self, filepath: str, container: Container, object_name: str, extra: dict = None) -> Object:
+        pass
+
+    @abstractmethod
+    def download_object(self, obj: Object, destination_path: str, overwrite_existing: bool = False) -> bool:
+        pass
+
+    @abstractmethod
+    def get_object(self, container: Container, name: str) -> Object:
+        pass
---
+++ neogit/object_storage/fake.py
@@ -0,0 +1,45 @@
+"""In-memory object storage adapter for testing"""
+
+import os
+from typing import Dict, Tuple
+
+from .abstract import (AbstractObjectStorage, Container, ContainerAlreadyExists, ContainerDoesNotExistError, Object,
+                       StorageDriver)
+
+
+class FakeObjectStorage(AbstractObjectStorage):
+    def __init__(self):
+        self._storage_driver = StorageDriver()
+        # obj name -> (data, metadata)
+        self._containers: Dict[Container, Dict[str, Tuple[bytes, Object]]] = {}
+
+    def create_container(self, name: str) -> Container:
+        if [c for c in self._containers.keys() if c.name == name]:
+            raise ContainerAlreadyExists
+        c = Container(name)
+        self._containers[c] = {}
+        return c
+
+    def get_container(self, name: str) -> Container:
+        try:
+            return [c for c in self._containers.keys() if c.name == name][0]
+        except IndexError:
+            raise ContainerDoesNotExistError
+
+    def upload_object(self, filepath: str, container: Container, object_name: str, extra: dict = None) -> Object:
+        with open(filepath, "rb") as f:
+            data: bytes = f.read()
+            size = os.stat(f.fileno()).st_size
+            obj = Object(object_name, size, "", container, extra, {})
+            self._containers[container][object_name] = (data, obj)
+            return obj
+
+    def download_object(self, obj: Object, destination_path: str, overwrite_existing: bool = False) -> bool:
+        with open(destination_path, "wb") as f:
+            data, metadata = self._containers[obj.container][obj.name]
+            f.write(data)
+            return True
+
+    def get_object(self, container: Container, name: str) -> Object:
+        _, obj = self._containers[container][name]
+        return obj
