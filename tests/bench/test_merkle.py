6c599d1faeac78e9679f9fc4739b25aeaf0198c9
Mathieu Tarral <mathieu.tarral@protonmail.com>
2021-05-12 21:30:57 +0200

    tests/bench: add tests to benchmark MerkleFSTree

--- tests/bench/test_merkle.py
+++ tests/bench/test_merkle.py
@@ -1,9 +1,23 @@
 from pathlib import Path
 
+from more_itertools import consume
+
 from neogit.merkle import MerkleFSTree
 
 
-def test_bench_merkle():
-    builder = MerkleFSTree(Path(""))
-    for _ in builder.merkelize():
-        pass
+def test_merkle_first(tmp_path, init_libcloud_object_storage_per_module, arg_repo_root):
+    """merklize a repository"""
+    assert arg_repo_root
+    ts_object = init_libcloud_object_storage_per_module
+    repo = Path(arg_repo_root)
+    builder = MerkleFSTree(repo, ts_object)
+    consume(builder.merkelize())
+
+
+def test_merkle_second(tmp_path, init_libcloud_object_storage_per_module, arg_repo_root):
+    """merklize a repository again, but with the same object storage, so objects are already indexed"""
+    assert arg_repo_root
+    ts_object = init_libcloud_object_storage_per_module
+    repo = Path(arg_repo_root)
+    builder = MerkleFSTree(repo, ts_object)
+    consume(builder.merkelize())
