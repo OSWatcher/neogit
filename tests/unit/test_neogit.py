4d62b1b7ef91822e0cf9db1ced39c0fdad9aed92
Mathieu Tarral <mathieu.tarral@protonmail.com>
2021-09-12 10:33:46 +0200

    tests: remove broken unit test

--- tests/unit/test_neogit.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-from neogit.service import Neogit
-
-
-def test_git_log(neo4j_con, fakefs_one_empty_file):
-    neogit = Neogit(Path("/"))
-    neogit.init()
-    neogit.commit("first_commit")
