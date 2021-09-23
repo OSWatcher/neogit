52d29c5db7fd14603f753f72f7945e417b8cf8db
Mathieu Tarral <mathieu.tarral@protonmail.com>
2021-09-23 23:30:00 +0200

    tests: add asdict methods to generate representation of Tree and TestTree

--- tests/model.py
+++ tests/model.py
@@ -1,3 +1,5 @@
+from typing import Dict, Union
+
 from neomodel import RelationshipTo, StringProperty, StructuredNode, StructuredRel
 
 
@@ -13,6 +15,17 @@ class Tree(StructuredNode):
     sha1sum = StringProperty(required=True, unique_index=True)
     children_tree = RelationshipTo("Tree", "HAS_CHILD_TREE", model=HasChildRel)
     children_blob = RelationshipTo(Blob, "HAS_CHILD_BLOB", model=HasChildRel)
+
+    def asdict(self) -> Dict[str, Union[str, Dict]]:
+        """Return a representation of the Tree as a dictionary"""
+        content = {}
+        for child_tree in self.children_tree:
+            rel = self.children_tree.relationship(child_tree)
+            content[rel.name] = {"sha1sum": child_tree.sha1sum, "content": child_tree.asdict()}
+        for child_blob in self.children_blob:
+            rel = self.children_blob.relationship(child_blob)
+            content[rel.name] = child_blob.sha1sum
+        return {"sha1sum": self.sha1sum, "content": content}
 
 
 class Commit(StructuredNode):
