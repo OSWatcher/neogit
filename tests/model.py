f6803849aac1779e3920a25dcdf19c059496ac1a
Mathieu Tarral <mathieu.tarral@protonmail.com>
2021-09-23 21:06:44 +0200

    tests/model: define Blob and Tree

--- tests/model.py
+++ tests/model.py
@@ -12,3 +12,13 @@ class Commit(StructuredNode):
 class Branch(StructuredNode):
     name = StringProperty(required=True)
     tracks = RelationshipTo(Commit, "TRACKS_COMMIT")
+
+
+class Blob(StructuredNode):
+    sha1sum = StringProperty(required=True, unique_index=True)
+
+
+class Tree(StructuredNode):
+    sha1sum = StringProperty(required=True, unique_index=True)
+    children_tree = RelationshipTo("Tree", "HAS_CHILD_TREE")
+    children_blob = RelationshipTo(Blob, "HAS_CHILD_BLOB")
