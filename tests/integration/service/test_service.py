b9adddc38e9c91c6ff8b7b30fbec262d6237d7a7
Mathieu Tarral <mathieu.tarral@protonmail.com>
2021-09-26 20:47:17 +0200

    tests/integration/service: add test_download_object_as_stream

--- tests/integration/service/test_service.py
+++ tests/integration/service/test_service.py
@@ -1,6 +1,8 @@
 """
 Test the Neogit service class
 """
+from functools import reduce
+
 import pytest
 
 from neogit.config import settings
@@ -105,3 +107,13 @@ def test_get_object_size(neogit_init):
     first_child_blob = next(iter(TEST_DATA_FS.dir_one_file.tree.children_blob.values()))
     size = neogit.get_object_size(first_child_blob.sha1sum)
     assert size == 75146
+
+
+# download_object_as_stream
+def test_download_object_as_stream(neogit_init):
+    neogit = neogit_init
+    neogit.commit("first commit", TEST_DATA_FS.dir_one_file.path)
+    first_child_blob_name, first_child_blob = next(iter(TEST_DATA_FS.dir_one_file.tree.children_blob.items()))
+    file_content = reduce(lambda a, b: a + b, neogit.download_object_as_stream(first_child_blob.sha1sum))
+    with open(TEST_DATA_FS.dir_one_file.path / first_child_blob_name, "rb") as f:
+        assert file_content == f.read()
