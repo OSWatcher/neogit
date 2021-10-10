2ea529b64d2f0682cd49b971f38e1c1e3701fdae
Mathieu Tarral <mathieu.tarral@protonmail.com>
2021-10-10 08:47:39 +0200

    tests/unit/merkle: add one test for object uploader

---
+++ tests/unit/merkle/test_uploader.py
@@ -0,0 +1,40 @@
+"""Test Object Uploader"""
+import hashlib
+from pathlib import Path
+
+from pytest import fixture
+
+from neogit.merkle.uploader import ObjectUploader
+from neogit.object_storage import FakeObjectStorage, TSObjectStorage
+
+
+@fixture
+def fake_ts_object_storage():
+    ts_object_storage = TSObjectStorage(FakeObjectStorage, None)
+    ts_object_storage.instance.create_container("objects")
+    yield ts_object_storage
+    # cleanup
+    for container in ts_object_storage.instance.iterate_containers():
+        ts_object_storage.instance.delete_container(container)
+
+
+def test_upload_one_file(fs, fake_ts_object_storage):
+    # arrange
+    expected_data = b"data"
+    hash = hashlib.sha1()
+    hash.update(expected_data)
+    expected_data_hash = hash.hexdigest()
+    filepath = Path("/file1.txt")
+    with open(filepath, "wb") as f:
+        f.write(expected_data)
+    container = fake_ts_object_storage.instance.get_container("objects")
+    # act
+    with ObjectUploader(fake_ts_object_storage) as uploader:
+        uploader.submit(filepath, expected_data_hash)
+    # assert
+    obj = fake_ts_object_storage.instance.get_object(container, expected_data_hash)
+    assert obj
+    fake_ts_object_storage.instance.download_object(obj, "/file2.txt")
+    with open("/file2.txt", "rb") as f:
+        content = f.read()
+        assert content, expected_data
