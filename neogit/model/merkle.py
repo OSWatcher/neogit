5c6b7663ed181d37aa106e658b97eff4ee2006ce
Mathieu Tarral <mathieu.tarral@protonmail.com>
2021-10-14 23:11:47 +0200

    model: update Tree

--- neogit/model/merkle.py
+++ neogit/model/merkle.py
@@ -1,4 +1,9 @@
+from functools import lru_cache
+from typing import Dict, Union
+
 from neomodel import RelationshipTo, StringProperty, StructuredNode, StructuredRel
+
+from neogit.core.model import MerkleLabel, MerkleNode
 
 
 class HasChildRel(StructuredRel):
@@ -21,5 +26,42 @@ class Blob(BaseMerkleNode):
 
 class Tree(BaseMerkleNode):
     hash = StringProperty(unique_index=True, required=True)
-    children_blob = RelationshipTo(Blob, "HAS_CHILD", model=HasChildRel)
-    children_tree = RelationshipTo("Tree", "HAS_CHILD", model=HasChildRel)
+    children_blob = RelationshipTo(Blob, "HAS_CHILD_BLOB", model=HasChildRel)
+    children_tree = RelationshipTo("Tree", "HAS_CHILD_TREE", model=HasChildRel)
+
+    @classmethod
+    def from_merkle_node(cls, node: MerkleNode) -> "Tree":
+        """Build a Tree from a MerkleNode"""
+        tree = cls(hash=node.hash)
+        # retrieve children
+        for child_name, child_node in node.children.items():
+            child_neo_node = cached_retrieve_merkle_node(child_node)
+            rel_properties = {"name": child_name}
+            if child_node.label == MerkleLabel.Blob:
+                tree.children_blob.connect(child_neo_node, rel_properties)
+            elif child_node.label == MerkleLabel.Tree:
+                tree.children_tree.connect(child_neo_node, rel_properties)
+            else:
+                raise NotImplementedError
+        return tree
+
+    def asdict(self) -> Dict[str, Union[str, Dict]]:
+        """Return a representation of the Tree as a dictionary"""
+        content = {}
+        for child_tree in self.children_tree:
+            rel = self.children_tree.relationship(child_tree)
+            content[rel.name] = {"hash": child_tree.hash, "content": child_tree.asdict()}
+        for child_blob in self.children_blob:
+            rel = self.children_blob.relationship(child_blob)
+            content[rel.name] = child_blob.hash
+        return {"hash": self.hash, "content": content}
+
+
+@lru_cache(maxsize=1024)
+def cached_retrieve_merkle_node(node: MerkleNode) -> Union[Blob, Tree]:
+    if node.label == MerkleLabel.Blob:
+        return Blob.nodes.get(hash=hash)
+    elif node.label == MerkleLabel.Tree:
+        return Tree.nodes.get(hash=hash)
+    else:
+        raise NotImplementedError
