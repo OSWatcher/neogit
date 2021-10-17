f15603bd59ba4a3d84f09f5b8d9d25d0d249760a
Mathieu Tarral <mathieu.tarral@protonmail.com>
2021-10-17 21:56:26 +0200

    tests/unit/core/visitor/test_thread: add basic test

---
+++ tests/unit/core/visitor/test_thread.py
@@ -0,0 +1,18 @@
+from queue import Queue
+
+from neogit.core.model import Node
+from neogit.core.visitor import NodeVisitor, NodeVisitorThread, VisitedNode
+
+
+def test_node_visitor_thread():
+    # arrange
+    queue = Queue()
+    visitor = NodeVisitor(queue)
+    node = Node()
+    # act
+    with NodeVisitorThread(visitor, node) as thread:
+        thread.start()
+        thread.join()
+    # assert
+    assert queue.qsize() == 1
+    assert queue.get() == VisitedNode(node, None)
