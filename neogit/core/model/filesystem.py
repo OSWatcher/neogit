d6905c041621fc3012807920f195a74b2dcadc53
Mathieu Tarral <mathieu.tarral@protonmail.com>
2021-11-01 21:11:15 +0100

    core/model/filesystem: handle every non file/symlink as empty file

--- neogit/core/model/filesystem.py
+++ neogit/core/model/filesystem.py
@@ -45,15 +45,18 @@ class FSFileNode(FSNode):
             raise ValueError(f"Path {value} should not be a directory")
 
     def hashable_data(self) -> Iterator[bytes]:
-        if self.path.is_symlink():
-            data: bytes = os.readlink(str(self.path)).encode()
-            bio = BytesIO(data)
-            read_block = partial(bio.read, 4096)
-            yield from iter(read_block, b"")
-        elif self.path.is_file():
+        if self.path.is_file():
             # file, return content
             with open(self.path, "rb") as f:
                 read_block = partial(f.read, 4096)
                 yield from iter(read_block, b"")
+        elif self.path.is_symlink():
+            data: bytes = os.readlink(str(self.path)).encode()
+            bio = BytesIO(data)
+            read_block = partial(bio.read, 4096)
+            yield from iter(read_block, b"")
         else:
-            raise NotImplementedError(f"file {self.path} of type {self.path.stat()} is not handled")
+            # treat as empty file
+            bio = BytesIO()
+            read_block = partial(bio.read, 4096)
+            yield from iter(read_block, b"")
