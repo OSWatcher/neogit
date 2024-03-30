5abcf97d5e3155683db14070e7e77cb10fc02e82
Mathieu Tarral <mathieu.tarral@protonmail.com>
2024-03-30 16:34:18 +0100

    visitor: define with attrs

--- neogit/core/visitor/node.py
+++ neogit/core/visitor/node.py
@@ -4,21 +4,25 @@ Visitor design pattern
 Taken from https://github.com/nodejs/node/blob/master/tools/inspector_protocol/jinja2/visitor.py (NodeJS)
 """
 
-import logging
 from functools import wraps
 from queue import Queue
-from typing import Any, Callable, List, Optional, Union
+from typing import Any, Callable, List, Optional
+import logging
 
-import attr
-from attr.validators import instance_of
+from attrs.validators import instance_of
+from attrs import define, field
 
 from neogit.core.model import Node
+from neogit.utils import DEFAULT_CLASS_LOGGER
 
 
-@attr.s
+@define(auto_attribs=True)
 class VisitedNode:
-    node: Node = attr.ib(validator=instance_of(Node))
-    return_value: Any = attr.ib()
+    """A node that has been visited by a visitor and the return value of the visit function."""
+    node: Node = field(validator=instance_of(Node))
+    """The node that was visited."""
+    return_value: Node = field()
+    """The return value of the visit function."""
 
 
 def visit_hook(f):
@@ -38,16 +42,17 @@ def visit_hook(f):
         if post_visit_f:
             post_visit_f(node, visit_ret_val, *args, **kwargs)
         # if queue defined, post visit return value there
-        if self._queue_list:
+        if self.queue_list:
             item = VisitedNode(node, visit_ret_val)
-            for q in self._queue_list:
+            for q in self.queue_list:
                 q.put(item)
         return visit_ret_val
 
     return wrapper
 
 
-class NodeVisitor(object):
+@define(auto_attribs=True)
+class NodeVisitor:
     """Walks the abstract syntax tree and call visitor functions for every
     node found.  The visitor functions may return values which will be
     forwarded by the `visit` method.
@@ -58,16 +63,12 @@ class NodeVisitor(object):
     (return value `None`) the `generic_visit` visitor is used instead.
     """
 
-    def __init__(self, queue: Optional[Union[Queue, List[Queue]]] = None):
-        """Initialize a NodeVisitor
+    logger: logging.Logger = field(default=DEFAULT_CLASS_LOGGER, init=False)
+    queue_list: List[Queue] = field(default=None)
 
-        Parameters:
-            queue: post visit queue to put visited node and their return value for external processing
-        """
-        self._logger = logging.getLogger(f"{self.__module__}.{self.__class__.__name__}")
-        self._queue_list = queue
-        if isinstance(queue, Queue):
-            self._queue_list = [queue]
+    def __attrs_post_init__(self):
+        if isinstance(self.queue_list, Queue):
+            self.queue_list = [self.queue_list]
 
     def get_visitor(self, node: Node, prefix="visit_") -> Optional[Callable]:
         """Return the visitor function for this node or `None` if no visitor
@@ -81,7 +82,7 @@ class NodeVisitor(object):
     def visit(self, node: Node, *args, **kwargs):
         """Visit a node."""
         f = self.get_visitor(node)
-        self._logger.debug("visit %s", node)
+        self.logger.debug("visit %s", node)
         if f is not None:
             return f(node, *args, **kwargs)
         return self.generic_visit(node, *args, **kwargs)
@@ -94,6 +95,6 @@ class NodeVisitor(object):
     def done_visiting(self):
         """a workaround method to put the None object inside the queue, if any"""
         # TODO: better interface ?
-        if self._queue_list:
-            for q in self._queue_list:
+        if self.queue_list:
+            for q in self.queue_list:
                 q.put(None)
