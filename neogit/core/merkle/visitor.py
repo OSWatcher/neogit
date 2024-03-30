791ac5b7ccfa939c24a4925dc59b081a60415a5e
Mathieu Tarral <mathieu.tarral@protonmail.com>
2024-03-30 14:34:24 +0100

    visitor: insert hash_obj into MerkleVisitor visit()

--- neogit/core/merkle/visitor.py
+++ neogit/core/merkle/visitor.py
@@ -1,34 +1,16 @@
+# need to import hashlib._Hash
+from __future__ import annotations
+
 import hashlib
 
-from more_itertools import consume
-
-from neogit.core.model import FSDirectoryNode, FSFileNode, MerkleLabel, MerkleNode
+from neogit.core.model.node import Node
 from neogit.core.visitor import NodeVisitor
 
 
 class MerkleVisitor(NodeVisitor):
-    """Visit a Node and build a MerkleTree"""
 
-    def visit_FSFileNode(self, node: FSFileNode, *args, **kwargs) -> MerkleNode:
+    # override visit method to define hashing algorithm
+    def visit(self, node: Node, *args, **kwargs):
+        # configure hashing algorithm here
         hash_obj = hashlib.sha1()
-        # iterate on the hashable bytes data and update the hash
-        consume(map(lambda data: hash_obj.update(data), node.hashable_data()))
-        # build merkle node and return it
-        merkle_node = MerkleNode(hash=hash_obj.hexdigest(), label=MerkleLabel.Blob)
-        return merkle_node
-
-    def visit_FSDirectoryNode(self, node: FSDirectoryNode, *args, **kwargs) -> MerkleNode:
-        hash_obj = hashlib.sha1()
-        # sort by 2 criterias
-        # - dir first
-        # - filename
-        # "not e.path.is_dir" because False is inferior to True and will be sorted first
-        merkle_children = {}
-        for child_node in sorted(node.iter_child_nodes(), key=lambda e: (not e.path.is_dir(), e.path.name)):
-            merkle_node = self.visit(child_node)
-            data = f"{child_node.path.name}{merkle_node.hash}\n".encode()
-            hash_obj.update(data)
-            merkle_children[child_node.path.name] = merkle_node
-        # compute final hash for this dir
-        merkle_node = MerkleNode(hash=hash_obj.hexdigest(), children=merkle_children, label=MerkleLabel.Tree)
-        return merkle_node
+        return super().visit(node, hash_obj, *args, **kwargs)
